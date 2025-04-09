814115ee2b815554bf2d3e8c4810011966531bd0
SERKAN SARIÇİÇEK <152647447+saricic@users.noreply.github.com>
2025-04-09 11:59:05 +0200

    Update test_sample.py
    multiply added

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -6,5 +6,12 @@ def test_sample_data(car_data):
  assert car_data["car"] == "mercedes"
  assert car_data["age"] == 2003
 
-
+@pytest.mark.parametrize("a, b, expected", [
+    (2, 3, 6),      # 2 * 3 = 6
+    (5, 0, 0),       # 5 * 0 = 0
+    (-4, 3, -12),    # -4 * 3 = -12
+    (2.5, 4, 10.0),  # 2.5 * 4 = 10.0
+])
+def test_multiply(a, b, expected):
+    assert multiply(a, b) == expected
   
