f0ed690711d54ae7a7c14ce083f3b1d678018fb8
SERKAN SARIÇİÇEK <152647447+saricic@users.noreply.github.com>
2025-04-09 11:53:57 +0200

    sdasd

--- test_sample.py
+++ test_sample.py
@@ -1,10 +1,1 @@
-import pytest
-@pytest.fixture
-def car_data():
- return {"car": "mercedes", "model": 2003}
-def test_sample_data(car_data):
- assert car_data["car"] == "mercedes"
- assert car_data["age"] == 2003
 
-
-  
