4fc1c9cbeb688505851ab74e699d893340cda8d9
SERKAN SARIÇİÇEK <152647447+saricic@users.noreply.github.com>
2025-04-09 11:54:17 +0200

    Update test_sample.py

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,42 +1,10 @@
 import pytest
 @pytest.fixture
-def sample_data():
- return {"name": "Alice", "age": 30}
-def test_sample_data(sample_data):
- assert sample_data["name"] == "Alice"
- assert sample_data["age"] == 30
+def car_data():
+ return {"car": "mercedes", "model": 2003}
+def test_sample_data(car_data):
+ assert car_data["car"] == "mercedes"
+ assert car_data["age"] == 2003
 
- @pytest.mark.parametrize("a, b, expected", [(2, 3, 5), (-1, 1, 0), (0, 0, 0)])
- def test_add_param(a, b, expected):
-  assert add(a, b) == e
 
-  name: Python
-  Test
-  Automation
-  on:
-  push:
-  branches:
-  - main
-  pull_request:
-
- jobs:
- test:
- runs - on: ubuntu - latest
- steps:
- - name: Checkout
- repository
- uses: actions / checkout @ v3
- - name: Set
- up
- Python
- uses: actions / setup - python @ v4
- with:
-  python - version: "3.10"
- - name: Install
- dependencies
- run: pip
- install
- pytest
- - name: Run
- tests
- run: pytest
+  
